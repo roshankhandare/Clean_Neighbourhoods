78659b75f593a2e87cab8777edc3d8f8bf675afa
roshankhandare <54729433+roshankhandare@users.noreply.github.com>
2019-11-10 20:40:23 -0500

    Schema

---
+++ SafeNeighbourhood/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace SafeNeighbourhood.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+                string propertyData = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/w3jp-dfxy.json");
+                List<QuickType.Property> allProperty = QuickType.Property.FromJson(propertyData);
+                IDictionary<string, QuickType.Property> propertDictionary = new Dictionary<string, QuickType.Property>();
+                foreach(QuickType.Property property in allProperty)
+                {
+                    propertDictionary.Add(property.StreetName, property);
+                }
+                ViewData["allcad"] = allProperty;
+            }
+
+        }
+    }
+}
