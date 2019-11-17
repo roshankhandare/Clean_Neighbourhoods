0ae42f5bed787a6a92ad2f4a43925c3beddf3ad0
roshankhandare <54729433+roshankhandare@users.noreply.github.com>
2019-11-17 12:26:36 -0500

    update v1.01

--- SafeNeighbourhood/Pages/Index.cshtml.cs
+++ SafeNeighbourhood/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using QuickTypeVProperty;
 
 namespace SafeNeighbourhood.Pages
 {
@@ -16,13 +16,9 @@ namespace SafeNeighbourhood.Pages
             using (var webClient = new WebClient())
             {
                 string propertyData = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/w3jp-dfxy.json");
-                List<QuickType.Property> allProperty = QuickType.Property.FromJson(propertyData);
-                IDictionary<string, QuickType.Property> propertDictionary = new Dictionary<string, QuickType.Property>();
-                foreach(QuickType.Property property in allProperty)
-                {
-                    propertDictionary.Add(property.StreetName, property);
-                }
-                ViewData["allcad"] = allProperty;
+
+                var property = VacantProperty.FromJson(propertyData);
+                ViewData["Welcome"] = property;
             }
 
         }
