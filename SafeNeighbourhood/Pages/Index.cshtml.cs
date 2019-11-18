b40e17ab683d8776301be1cdbc434a5386d0b6c6
Nikita Ahuja <ahujans@mail.uc.edu>
2019-11-18 14:43:48 -0500

    Code Review (11/18/2018) Changes 1. Index.cshtml (Line #22): Removed @ { } 2. Index.cshtml (Line #13): added class = “table” in table tag to increase readability. 3. Index.cshtml.cs (Line #18): Created a method for getting data from Json. 4. Index.cshtml.cs : Added code for Json Validation. 5. Index.cshtml.cs: Changed data type of property from var to VacantProperty[]

--- SafeNeighbourhood/Pages/Index.cshtml.cs
+++ SafeNeighbourhood/Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using QuickTypeVProperty;
 
 namespace SafeNeighbourhood.Pages
@@ -15,13 +17,38 @@ namespace SafeNeighbourhood.Pages
         {
             using (var webClient = new WebClient())
             {
-                string propertyData = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/w3jp-dfxy.json");
+                string propertyData = GetData("https://data.cincinnati-oh.gov/resource/w3jp-dfxy.json"); // using a method here for flexibility
 
-                var property = VacantProperty.FromJson(propertyData);
-                ViewData["property"] = property;
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("Schema/VacantProperty.json"));
+                JArray JsonArray = JArray.Parse(propertyData);
+
+                IList<string> validationEvents = new List<string>();
+
+                if (JsonArray.IsValid(schema))
+                {
+                    VacantProperty[] property = VacantProperty.FromJson(propertyData);
+                    ViewData["property"] = property;
+                } else
+                {
+                    foreach (string evt in validationEvents)
+                    {
+                        Console.WriteLine(evt);
+                    }
+                    ViewData["property"] = new List<VacantProperty>();
+                }
 
             }
 
+        }
+
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endpoint);
+            }
+            return downloadedData;
         }
     }
 }
